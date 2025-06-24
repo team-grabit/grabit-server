09077f3a2c7221fb9250dfc5196873197e0aa09e
jinyong68 <163990191+jinyong68@users.noreply.github.com>
2025-06-24 14:42:46 +0900

    feat/dto

--- src/main/java/me/cher1shrxd/grabit/service/CartService.java
+++ src/main/java/me/cher1shrxd/grabit/service/CartService.java
@@ -1,5 +1,7 @@
 package me.cher1shrxd.grabit.service;
 
+import me.cher1shrxd.grabit.dto.req.CartRequestDTO;
+import me.cher1shrxd.grabit.entity.CartEntity;
 import me.cher1shrxd.grabit.repository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,10 @@ public class CartService {
     @Autowired
     private CartRepository cartRepository;
 
+    public boolean createCart(CartRequestDTO cartRequestDTO) {
+        CartEntity cartEntity = new CartEntity();
+        cartEntity.setName(cartRequestDTO.getName());
+        CartEntity saved = cartRepository.save(cartEntity);
+        return saved != null;
+    }
 }
