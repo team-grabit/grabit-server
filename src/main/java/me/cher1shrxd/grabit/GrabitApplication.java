daf883e51ba478106e63c90f22939e2806142a4d
김태우 <taewoo080401@gmail.com>
2025-06-24 13:40:12 +0900

    init

---
+++ src/main/java/me/cher1shrxd/grabit/GrabitApplication.java
@@ -0,0 +1,13 @@
+package me.cher1shrxd.grabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrabitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/cher1shrxd/grabit/GrabitApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cher1shrxd.grabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
